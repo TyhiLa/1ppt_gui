ccb0d05bebd204d74ae343114d81c46ef43a33e6
hoshimaemi <wb2811508269@163.com>
2021-05-23 08:22:36 +0800

    Add templates

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("dfs")
+# -*- coding: utf-8 -*- 
+"""
+Project: ppt
+Creator: hoshimaemi
+Create time: 2021-05-23 08:22
+IDE: PyCharm
+Introduction:
+"""
+import tkinter as tk
+
+
+class Main(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("1024x800")
+        self.title("第一ppt下载")
+
+    def setWigets(self):
+        pass
