894f1dff48e895571c5b588fb1dbdcc677017252
hoshimaemi <wb2811508269@163.com>
2021-05-23 10:13:30 +0800

    Add templates

--- main.py
+++ main.py
@@ -14,6 +14,19 @@ class Main(tk.Tk):
         super().__init__()
         self.geometry("1024x800")
         self.title("第一ppt下载")
+        self.resizable(width=False, height=False)
+        self.e1text = tk.StringVar()
+        self.e1text.set("输入所选内容")
+        self.setwigets()
 
-    def setWigets(self):
-        pass
+    def setwigets(self):
+        entry1  = tk.Entry(self, textvariable=self.e1text)
+        entry1.pack()
+        tk.Button(self, text="下载", command=self.btn1) .pack()
+
+    def btn1(self):
+        print(self.e1text.get())
+
+
+if __name__ == '__main__':
+    Main().mainloop()
