a4697af175d59707c7dae965dd1d36404cff776d
LZH200 <372462393@QQ.COM>
2021-06-04 12:55:19 +0800

    This is a command version

--- command_version.py
+++ command_version.py
@@ -41,9 +41,7 @@ def file_download(url):
         second_link = req_for_xpath(x, '/html/body/div[4]/div[1]/dl/dd/ul[1]/li/a/@href')[0]
         download_url = req_for_xpath(url_all + second_link, '/html/body/dl/dd/ul[2]/li[1]/a/@href')[0]
         url_list.append(download_url)
-    name = req_for_xpath(url, '/html/body/div[5]/dl/dd/ul//li/a/img/@alt')
-    file_dict = dict(zip(name, url_list))
-    return file_dict
+    return url_list
 
 
 def get_the_format(url, page):
@@ -56,28 +54,40 @@ def get_the_max_page(url):
     return max_link.split('_')[-1][:-5]
 
 
+def auto_rename(old, new):
+    x = 0
+    while 1:
+        if x == 0:
+            try:
+                rename(old, new)
+            except FileExistsError:
+                x += 1
+                pass
+        else:
+            try:
+                rename(old, str(new).split('.')[0] + f'({x}).' + str(new).split('.')[1])
+                break
+            except FileExistsError:
+                x += 1
+                pass
+
+
 choice_link = url_all + get_choice_link(choice)
 for a in range(1, int(get_the_max_page(choice_link))):
     add_page = choice_link + get_the_format(choice_link, a)
-    file_dict1 = file_download(add_page)
-    with tqdm(total=len(file_dict1)) as bar:
+    file_list = file_download(add_page)
+    with tqdm(total=len(file_list)) as bar:
         bar.set_description(f'第{a}页下载中')
-        for (key, value) in file_dict1.items():
-            req_file = get(value, headers=headers).content
-            with open(key + '.zip', 'wb') as f:
+        for link in file_list:
+            req_file = get(link, headers=headers).content
+            with open('log' + '.zip', 'wb') as f:
                 f.write(req_file)
-            zip_file = zipfile.ZipFile(key + '.zip')
+            zip_file = zipfile.ZipFile('log' + '.zip')
             for names in zip_file.namelist():
                 gbk_names = str(names).encode('cp437').decode('GBK')
                 if '.ppt' in names:
-                    m = 0
                     zip_file.extract(names)
-                    try:
-                        rename(str(names), gbk_names)
-                    except FileExistsError:
-                        rename(str(names), gbk_names.split('.')[0] + f'{m + 1}' + gbk_names.split('.')[1])
-                else:
-                    pass
+                    auto_rename(str(names), gbk_names)
             zip_file.close()
-            remove(key + '.zip')
+            remove('log' + '.zip')
             bar.update(1)
