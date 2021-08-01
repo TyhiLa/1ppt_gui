dddeb7e430fa3a028b2aea617809cb71371184f4
mrdiamond <hoshimaemi@outlook.com>
2021-08-01 15:44:14 +0800

    remove spder.py

--- spider.py
+++ spider.py
@@ -26,67 +26,7 @@ def xpath(req_in, xpath_road):
     return selector_re
 
 
-def get_choice_link(choose_title, all_choose_link):
-    choice_link1 = all_choose_link[choose_title - 1]
-    return choice_link1
 
-
-def file_download(url):
-    web = req_for_web(url)
-    file_first_link_list = xpath(web, '/html/body/div[5]/dl/dd/ul//li/h2/a/@href')
-    url_list = []
-    for x in [url_all + i1 for i1 in file_first_link_list]:
-        second_link = xpath(req_for_web(x), '/html/body/div[4]/div[1]/dl/dd/ul[1]/li/a/@href')[0]
-        download_url = xpath(req_for_web(url_all + second_link), '/html/body/dl/dd/ul[2]/li[1]/a/@href')[0]
-        url_list.append(download_url)
-    return url_list
-
-
-def format_link(web, page):
-    page_link = xpath(web, '/html/body/div[5]/dl/dd/div[2]/ul/li[3]/a/@href')[0]
-    return '/' + page_link[:-6] + str(page) + '.html'
-
-
-def get_the_max_page(web):
-    max_link = xpath(web, '/html/body/div[5]/dl/dd/div[2]/ul/li[16]/a/@href')[0]
-    return max_link.split('_')[-1][:-5]
-
-
-def auto_decode(name):
-    gbkname = str(name).encode('cp437').decode('gbk')
-    x = 1
-    while True:
-        if path.isfile(gbkname):
-            gbkname = str(gbkname).split('.')[0] + f'{x}.' + str(gbkname).split('.')[1]
-            x += 1
-        else:
-            break
-    rename(name, gbkname)
-
-# Get $choice from ListBox which binded <Double click>
-def extract(choice, all_choose_link):
-    choice_link = url_all + get_choice_link(choice, all_choose_link)
-    web_for_page = req_for_web(choice_link)
-    for a in range(1, int(get_the_max_page(web_for_page))):
-        add_page = choice_link + format_link(web_for_page, a)
-        file_list = file_download(add_page)
-        with tqdm(total=len(file_list)) as bar:
-            bar.set_description(f'第{a}页下载中')
-            for link in file_list:
-                req_file = get(link, headers=headers).content
-                with open('log' + '.zip', 'wb') as f:
-                    f.write(req_file)
-                zip_file = zipfile.ZipFile('log' + '.zip')
-                for names in zip_file.namelist():
-                    if '.ppt' in names:
-                        zip_file.extract(names)
-                        auto_decode(names)
-                zip_file.close()
-                remove('log' + '.zip')
-                bar.update(1)
-
-# return essential value for display
-# TODO:Need every artical titles and links
 def getTitleLink(url, index):
     html = xpath(req_for_web(url), '/html/body/div[5]/dl/dd/ul/li')
     # print(html)
@@ -95,10 +35,8 @@ def getTitleLink(url, index):
     # img = [re.findall('<img src="(.*?)"', x)[0] for x in html]
     temp = [re.findall('<img src="(.*?)" alt="(.*?)">', x)[0] for x in html]
     dow_url = [url_all + re.findall('<a href="(.*?)"', x)[0] for x in html]
-    print(dow_url)
     title = [x[1] for x in temp]
     img_url = [x[0] for x in temp]
-    print(img_url)
     return title, img_url[index], dow_url
 
 
