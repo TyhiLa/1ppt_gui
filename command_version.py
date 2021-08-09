570ef5622fd50fcba4b08569fe67e77e52ae3f2a
LZH200 <372462393@qq.com>
2021-08-09 04:15:37 +0000

    update command_version.py.

--- command_version.py
+++ command_version.py
@@ -6,7 +6,6 @@ import re
 import io
 import asyncio
 import aiohttp
-import time
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                          'Chrome/90.0.4430.212 Safari/537.36'}
